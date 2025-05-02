a1a081ccada4fc67003129afd4f57477d78bd3c4
Alvy Depositar <78188626+alvydepositar@users.noreply.github.com>
2025-05-02 11:20:42 +0800

    PostgreSQL to SQLite
    -- changed database to SQLIte from PostgreSQL.

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'inventory_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -84,7 +85,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
